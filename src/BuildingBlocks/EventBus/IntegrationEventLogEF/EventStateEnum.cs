42dd9c84325e5e64f4559c7f07340dca6f035e1c
dogaanismail <ismaildogaan@gmail.com>
2022-04-30 02:57:28 +0300

    eCommerceOnContainer - IntegrationEventLogEF class library has been added into the solution.

---
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/EventStateEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.eCommerceOnContainers.BuildingBlocks.IntegrationEventLogEF;
+
+public enum EventStateEnum
+{
+    NotPublished = 0,
+    InProgress = 1,
+    Published = 2,
+    PublishedFailed = 3
+}
---
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.eCommerceOnContainers.BuildingBlocks.IntegrationEventLogEF.Services;
+public interface IIntegrationEventLogService
+{
+    Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+    Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction);
+    Task MarkEventAsPublishedAsync(Guid eventId);
+    Task MarkEventAsInProgressAsync(Guid eventId);
+    Task MarkEventAsFailedAsync(Guid eventId);
+}
