a77141636d8ae7002fa207dff9a409cdae5f76d0
dogaanismail <ismaildogaan@gmail.com>
2022-05-30 01:33:45 +0300

    eCommerceOnContainer - WebSPA has been added into the project.

---
+++ src/Web/WebSPA/Server/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+namespace eCommerceConContainers.WebSPA.Server.Controllers;
+
+public class HomeController : Controller
+{
+    #region Fields
+
+    private readonly IWebHostEnvironment _env;
+    private readonly IOptionsSnapshot<AppSettings> _settings;
+
+    #endregion
+
+    #region Ctor
+    public HomeController(IWebHostEnvironment env, 
+        IOptionsSnapshot<AppSettings> settings)
+    {
+        _env = env;
+        _settings = settings;
+    }
+    #endregion
+
+    #region Methods
+    public IActionResult Configuration()
+    {
+        return Json(_settings.Value);
+    }
+
+    #endregion
+}
